7ca90d414100156139b122fa6981ae6f19bf72cc
Ammar Khan <ammar.khan@ubisoft.com>
2024-09-29 20:16:13 +0530

    added new album

--- qtify/src/Components/Section/Section.js
+++ qtify/src/Components/Section/Section.js
@@ -7,14 +7,15 @@ import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 import Cards from '../Cards/Cards';
 import Carousel from './Carousel';
+import { useEffect } from 'react';
 
 function Accordian() {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState('panel1');
   const [songData, setSongData] = useState([])
   
-  const fetchData = async () =>{
+  const fetchData = async (url) =>{
     try {
-      const response = await axios.get(`https://qtify-backend-labs.crio.do/albums/top`)
+      const response = await axios.get(`https://qtify-backend-labs.crio.do/albums/${url}`)
       console.log(response)
       const data = response.data
       setSongData(data)
@@ -23,18 +24,26 @@ function Accordian() {
     }
   };
   
+useEffect(() => {
+    fetchData("top")
+},[])
+
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
     if(isExpanded){
-      fetchData()
+      if(panel === "panel1")
+      fetchData("top")
+    }
+    if(panel === "panel2"){
+      fetchData("new")
     }
   };
 
   return (
 
       <div>
-      <Accordion sx={{backgroundColor: 'darkgrey', fontFamily: 'Poppins'}}expanded={expanded === 'panel1'} onChange={handleChange('panel1')} defaultExpanded>
+      <Accordion sx={{backgroundColor: 'darkgrey', fontFamily: 'Poppins'}} expanded={expanded === 'panel1'} onChange={handleChange('panel1')} defaultExpanded>
         <AccordionSummary
           expandIcon={expanded === 'panel1' ? 'Collapse All' : 'Show'}
         >
@@ -44,6 +53,19 @@ function Accordian() {
             <Carousel songData={songData}/>
         </AccordionDetails>
       </Accordion>
+
+
+      <Accordion sx={{ backgroundColor: 'darkgrey', fontFamily: 'Poppins' }} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <AccordionSummary
+          expandIcon={expanded === 'panel2' ? 'Collapse All' : 'Show'}
+        >
+          <Typography>New Albums</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Carousel songData={songData} />
+        </AccordionDetails>
+      </Accordion>
+
     </div>
   )
 }
